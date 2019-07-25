c7135e0201089e1e8eff1583d38a3d96e034e87c
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2019-07-25 20:46:02 +0800

    0.

--- Scenario generation/ObjFun.m
+++ Scenario generation/ObjFun.m
@@ -1,17 +1,9 @@
 function obj = ObjFun(x)
 
-% mean = 467.25;
-% variance = 99.42;
-% skew = 1.06;
-% kurt = 4.35;
-
-mean = 33.82;
-variance = 175.42;
-skew = 0.25;
-kurt = 2.78;
-
-
-z
+mean = 467.25;
+variance = 99.42;
+skew = 1.06;
+kurt = 4.35;
 
 T = length(x)/2;
 d = x(1:T);
--- Scenario generation/ScenarioGeneration.m
+++ Scenario generation/ScenarioGeneration.m
@@ -4,39 +4,47 @@ function ScenarioGeneration
 % Management Science
 %   
 
-% mean = 467.25;
-% variance = 99.42;
-mean = 33.82;
-variance = 175.42;
+% mean = 467.25
+% variance = 99.42
+% skewness = 1.06
+% kurosis = 4.35
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 iniDemand = [50, 300, 100, 80, 600];
 =======
 iniDemand = ones(1, 5) * mean/5;
 >>>>>>> 9b61c7797d5db529eac904f64871378c22468c06
+=======
+iniDemand = [400, 300, 100, 500, 600];
+>>>>>>> parent of 9b61c77... update scenario realization
 iniPossibility = [0.2, 0.2, 0.2, 0.2, 0.2];
 
 T = length(iniDemand);
-worstDemand = mean - 2.5*sqrt(variance);
-% Aeq =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1; 1, zeros(1, 2*T - 1); zeros(1, T), 1, zeros(1, T-1)];
-% beq = [1; worstDemand; 0.01];
 Aeq =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
 beq = 1;
+<<<<<<< HEAD
 <<<<<<< HEAD
 ub = [10000*ones(1,T), 0.5*ones(1, T)];
 [x, fval] = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, ub);
 =======
 [x, fval] = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, []);
 >>>>>>> 9b61c7797d5db529eac904f64871378c22468c06
+=======
+x = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, []);
+>>>>>>> parent of 9b61c77... update scenario realization
 d = x(1 : T);
 p = x(T + 1 : 2*T);
 d
 p
 <<<<<<< HEAD
+<<<<<<< HEAD
 fprintf('fit value = %.2f\n', fval);
 =======
 fprintf('fit gap = %.2f\n', fval);
 >>>>>>> 9b61c7797d5db529eac904f64871378c22468c06
+=======
+>>>>>>> parent of 9b61c77... update scenario realization
 fprintf('mean = %.2f\n', Mean(d, p));
 fprintf('variance = %.2f\n', Variance(d, p));
 fprintf('skew = %.2f\n', Skew(d, p));
