03168a0248216a5d4af05866c16593a6e5f25353
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2019-07-25 20:47:07 +0800

    0.

--- Scenario generation/ScenarioGeneration.m
+++ Scenario generation/ScenarioGeneration.m
@@ -1,6 +1,4 @@
-function ScenarioGeneration
-% this a matlab test code for getting the demand realizaions in the scenario tree. This 
-% method is adopted from the paper in Hoyland and Wallace (2001),
+@@ -4,22 +4,26 @@ function ScenarioGeneration
 % Management Science
 %   
 
@@ -8,46 +6,28 @@ function ScenarioGeneration
 % variance = 99.42
 % skewness = 1.06
 % kurosis = 4.35
+% mean = 467.25;
+% variance = 99.42;
+mean = 33.82;
+variance = 175.42;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-iniDemand = [50, 300, 100, 80, 600];
-=======
-iniDemand = ones(1, 5) * mean/5;
->>>>>>> 9b61c7797d5db529eac904f64871378c22468c06
-=======
 iniDemand = [400, 300, 100, 500, 600];
->>>>>>> parent of 9b61c77... update scenario realization
+iniDemand = ones(1, 5) * mean/5;
 iniPossibility = [0.2, 0.2, 0.2, 0.2, 0.2];
 
 T = length(iniDemand);
+worstDemand = mean - 2.5*sqrt(variance);
+% Aeq =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1; 1, zeros(1, 2*T - 1); zeros(1, T), 1, zeros(1, T-1)];
+% beq = [1; worstDemand; 0.01];
 Aeq =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
 beq = 1;
-<<<<<<< HEAD
-<<<<<<< HEAD
-ub = [10000*ones(1,T), 0.5*ones(1, T)];
-[x, fval] = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, ub);
-=======
-[x, fval] = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, []);
->>>>>>> 9b61c7797d5db529eac904f64871378c22468c06
-=======
 x = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, []);
->>>>>>> parent of 9b61c77... update scenario realization
+[x, fval] = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, []);
 d = x(1 : T);
 p = x(T + 1 : 2*T);
 d
 p
-<<<<<<< HEAD
-<<<<<<< HEAD
-fprintf('fit value = %.2f\n', fval);
-=======
 fprintf('fit gap = %.2f\n', fval);
->>>>>>> 9b61c7797d5db529eac904f64871378c22468c06
-=======
->>>>>>> parent of 9b61c77... update scenario realization
 fprintf('mean = %.2f\n', Mean(d, p));
 fprintf('variance = %.2f\n', Variance(d, p));
 fprintf('skew = %.2f\n', Skew(d, p));
-fprintf('kurt = %.2f\n', Kurt(d, p));
-end
-
