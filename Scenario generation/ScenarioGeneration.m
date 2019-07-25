e1b5c0c8edfb897fa6cb13d6c57f6651559293ac
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2019-07-25 20:43:51 +0800

    ..

--- Scenario generation/ScenarioGeneration.m
+++ Scenario generation/ScenarioGeneration.m
@@ -15,10 +15,22 @@ iniPossibility = [0.2, 0.2, 0.2, 0.2, 0.2];
 T = length(iniDemand);
 Aeq =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
 beq = 1;
+<<<<<<< HEAD
 x = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, zeros(1, 2*T),[]);
 d = x(1 : T);
 p = x(T + 1 : 2*T);
 disp(d);
 disp(p);
+=======
+x = fmincon(@(x)ObjFun(x), [iniDemand, iniPossibility], [], [], Aeq, beq, ones(1, 2*T)*0.01, []);
+d = x(1 : T);
+p = x(T + 1 : 2*T);
+d
+p
+fprintf('mean = %.2f\n', Mean(d, p));
+fprintf('variance = %.2f\n', Variance(d, p));
+fprintf('skew = %.2f\n', Skew(d, p));
+fprintf('kurt = %.2f\n', Kurt(d, p));
+>>>>>>> parent of 9b61c77... update scenario realization
 end
 
