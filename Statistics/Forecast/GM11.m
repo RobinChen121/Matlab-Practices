72b4223ddcaa83aa8c389bef9e3fd22a17863747
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2019-07-31 19:36:04 +0800

    add forecast GM11 model

---
+++ Statistics/Forecast/GM11.m
@@ -0,0 +1,40 @@
+function GM11
+% 灰色预测 GM(1, 1)模型，比较适合指数趋势的数据
+%
+
+close all;
+Y0 = [394	7269	3954	1723]; % 原始数据
+Y1 = cumsum(Y0);
+X = Y0(2 : end)';
+N = length(Y0);
+B = zeros(N - 1, 2);
+for i = 1 : N - 1
+    B(i, 1) = -0.5 * (Y1(i) + Y1(i + 1));
+    B(i, 2) = 1;
+end
+alB = inv(B'*B)*B'*X;
+alpha = alB(1);
+mu= alB(2);
+
+Y2 = zeros(1, N + 1);
+Y2(1) = Y0(1);
+for i = 2 : N + 1
+    Y2(i) = (Y0(1) - mu/alpha) * exp(-alpha * (i-1)) + mu/alpha;
+end
+foreValue = zeros(1, N + 1);
+foreValue(1) = Y0(1);
+for i = 2 : N + 1
+    foreValue(i) = Y2(i) - Y2(i - 1);
+end
+fprintf('forecast value is %.2f\n', foreValue(end));
+
+%绘制曲线图
+t1 = 1 : N;
+t2 = 1 : N + 1;
+
+
+plot(t1, Y0,'ro'); hold on;
+plot(t2, foreValue, 'g-');
+legend('实际数据','预测数据');
+grid on;
+end
