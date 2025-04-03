b36f7954f5fb20219c056b71f2eb2ff33a246265
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2025-04-03 19:32:48 +0100

    update

--- Draw pictures/RestrictLineConvex.m
+++ Draw pictures/RestrictLineConvex.m
@@ -8,12 +8,12 @@ hold on;
 x0 = 0;
 v = 2;
 fplot(@(t) (x0 + t*v).^2, [-2 2]);
-v = 0.5;
-fplot(@(t) (x0 + t*v).^2, [-2 2]);
-x0 = 1;
-v = 2;
-fplot(@(t) (x0 + t*v).^2, [-2 2]);
-legend('f(x)=x^2', 'f(t)=f(x+tv)=4t^2', 'f(t)=f(x+tv)=0.25t^2', 'f(t)=f(x+tv)=(1+2t)^2', 'Location', 'north')
+% v = 0.5;
+% fplot(@(t) (x0 + t*v).^2, [-2 2]);
+% x0 = 1;
+% v = 2;
+% fplot(@(t) (x0 + t*v).^2, [-2 2]);
+legend('f(x)=x^2', 'f(t)=f(x+tv)=4t^2', 'Location', 'north')
 
 figure(2);
 f = @(x, y) x.^2 + y.^2;
@@ -31,6 +31,11 @@ patch([-5 -5 5 5], [-5 -5 5 5], [-10 50 50 10], 'r')
 figure(3)
 fplot(@(t) (2*t).^2, [-2 2]);
 title('$f(t)=f(x+tv)=2t^2$', 'interpreter', 'latex')
+
+figure(4);
+f = @(x, y) x.^2 + y.^2;
+fmesh(f)
+title('$f(x) = x_1^2+x_2^2$', 'interpreter', 'latex')
 end
 
 
